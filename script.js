65ecdb93a88c7837072d8faafc960780b9c57a3d
Sai Pratim Mishra <164091269+saipratimmishra@users.noreply.github.com>
2025-07-12 19:18:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+document.querySelectorAll('a.nav-link').forEach(link => {
+  link.addEventListener('click', function (e) {
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
+      e.preventDefault();
       target.scrollIntoView({ behavior: 'smooth' });
     }
   });
